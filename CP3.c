73185a42ec8e142081ddddf623dfac59ac2f6dda
gameloque <gabrielhenriqueff1448@gmail.com>
2026-05-11 11:28:30 -0300

    Código Inicial

---
+++ CP3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h> 
+ float saldo = 0; 
+ int usuario, opcao; 
+ int main(){ 
+    printf(" BEM VINDO AO SISTEMA DE CAIXA ELETRONICO FIAP\n"); 
+    printf(" SEU SALDO E DE: R$%.2f ", saldo); 
+    
+
+
+    return 0; 
+}
